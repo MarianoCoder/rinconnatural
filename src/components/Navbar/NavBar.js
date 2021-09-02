422a18b2b542c61ca2100b64683a23b57c77f2ef
Mariano Orona <maoaltacba@gmail.com>
2021-09-01 22:00:28 -0300

    Creacion de nav

---
+++ src/components/Navbar/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NavBar.css'
+
+function NavBar() {
+    return (
+        <div className="navbar">
+        <div>Rincón Natural</div>
+      <div className="menu">
+        <li>Inicio</li>
+        <li>Tienda</li>
+        <li>Contacto</li>
+      </div>
+        </div>
+    )
+}
+
+export default NavBar;
