889d5308241980668bbb6be557fcd1731361cf91
Mariano Orona <maoaltacba@gmail.com>
2021-09-13 21:56:50 -0300

    Final contador

--- src/App.js
+++ src/App.js
@@ -17,11 +17,7 @@ function App() {
       <Header name="Tienda"/>
       <NavBar />
       <ItemListContainer />
-<<<<<<< HEAD
       <ItemCount stock="5" initial="1"/>
-=======
-      <ItemCount stock="5" initial="0"/>
->>>>>>> a1ca6d8d526c625383ea5a75d44a481071da0c38
     </div>
   );
 }
