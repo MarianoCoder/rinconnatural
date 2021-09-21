ce145392d464f8c551881530194edc3ea11b8009
Mariano Orona <maoaltacba@gmail.com>
2021-09-20 21:55:41 -0300

    Desafio ItemDetail terminado final

--- src/containers/ItemDetailContainer.jsx
+++ src/containers/ItemDetailContainer.jsx
@@ -1,32 +1,12 @@
 import * as React from "react";
 import ItemDetail from "../components/ItemDetail/ItemDetail";
 
-<<<<<<< HEAD
-
-
-
 const ItemDetailContainer = () =>{
-    const [data, setData] = React.useState([]);
-    const [loading, setLoading] = React.useState(false);
-    const [error, setError] = React.useState(null);
-
-    
-=======
-const ItemDetailContainer = () =>{
-<<<<<<< HEAD
     //const [data, setData] = React.useState([]);
    // const [loading, setLoading] = React.useState(false);
    // const [error, setError] = React.useState(null)
    // React.useEffect(()=>{
      //   const url ="http://localhost:3001/products/";
-=======
-    const [data, setData] = React.useState([]);
-    const [loading, setLoading] = React.useState(false);
-    const [error, setError] = React.useState(null)
->>>>>>> f1683f5606cd9fe9b9749d33908a0b8ae0040a48
-    React.useEffect(()=>{
-        const url ="http://localhost:3001/products/";
->>>>>>> 0abc214c929855775b9b2fc89401d6f409a6256b
         
      //   setLoading(true);
       //  fetch(url)
